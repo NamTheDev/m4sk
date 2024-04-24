d084468075a27839bc38c54ffc91521a50683fc4
KainTheDev <thica.doi@gmail.com>
2024-04-24 17:46:44 +0700

    .

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ const { readFileSync } = require("fs");
 module.exports = {
   prefix: ">",
   owner: "1202507536838828083",
-  token: readFileSync('src/token.txt', 'utf-8')
+  token: readFileSync('token.txt', 'utf-8')
 }
